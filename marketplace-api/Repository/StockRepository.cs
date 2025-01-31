ea926cbccc1c566dbb1decd637cd9c6b0388c832
vchandra22 <vincentchaandra@gmail.com>
2025-01-31 10:19:39 +0700

    feat: pagination, page number, page size

--- marketplace-api/Repository/StockRepository.cs
+++ marketplace-api/Repository/StockRepository.cs
@@ -38,7 +38,9 @@ public class StockRepository : IStockRepository
             }
         }
         
-        return await stocks.ToListAsync();
+        var skipNumber = (query.PageNumber - 1) * query.PageSize;
+        
+        return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
     }
     
     public async Task<Stock?> GetByIdAsync(Guid id)
